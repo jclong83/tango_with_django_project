cff8071f49ae57bb35b1bd6683772424eff0b275
James Long <jalong@blizzard.com>
2016-06-01 11:52:09 +0800

    added about template/rango models

--- rango/views.py
+++ rango/views.py
@@ -13,4 +13,5 @@ def index(request):
     return render(request, 'rango/index.html', context=context_dict)
 
 def about(request):
-    return HttpResponse('Rango says here is the about page. <a href="/rango/">Back Home</a>')
+    context_dict = {'author': 'James Long', 'boldmessage': 'Everything you wanted to know about Rango!'}
+    return render(request, 'rango/about.html', context=context_dict)
