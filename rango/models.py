727839c51f347af45eacfaebc0d8e760c749ac1b
James Long <jalong@blizzard.com>
2016-06-04 12:07:42 +0800

    updated models and add db population script

--- rango/models.py
+++ rango/models.py
@@ -2,7 +2,12 @@ from django.db import models
 
 class Category(models.Model):
     name = models.CharField(max_length=128, unique=True)
-    
+    views = models.IntegerField(default=0)
+    likes = models.IntegerField(default=0)
+
+    class Meta:
+        verbose_name_plural = 'categories'
+
     def __str__(self):
         return self.name
 
@@ -11,8 +16,6 @@ class Page(models.Model):
     title = models.CharField(max_length=128)
     url = models.URLField()
     views = models.IntegerField(default=0)
-    
+
     def __str__(self):
         return self.title
-
-
