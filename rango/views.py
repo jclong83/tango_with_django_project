9714ae577045eca4c11437c214724fff45d11f2b
James Long <jalong@blizzard.com>
2016-06-06 15:44:08 +0800

    adding slug field to Category, updating index template

--- rango/views.py
+++ rango/views.py
@@ -2,14 +2,20 @@ from django.shortcuts import render
 
 from django.http import HttpResponse
 
-def index(request):
-    # Construct a dictionary to pass to the template engine as its context.
-    # Note the key boldmessage is the same as {{ boldmessage }} in the template!
-    context_dict = {'boldmessage': 'Crunchy, creamy, cookie, candy, cupcake!'}
+# Import the Category model
+from rango.models import Category
 
-    # Return a rendered response to send to the client.
-    # We make use of the shortcut function to make our lives easier.
-    # Note that the first parameter is the template we wish to use.
+def index(request):
+    # Query the database for a list of ALL categories currently stored.
+    # Order the categories by no. of likes in descending order.
+    # Retrieve the top 5 only - or all if less than 5.
+    # Place the list in our context_dict dictionary
+    # that will be passed to the template engine.
+
+    category_list = Category.objects.order_by('-likes')[:5]
+    context_dict = {'categories': category_list}
+
+    # Render the request and send it back!
     return render(request, 'rango/index.html', context=context_dict)
 
 def about(request):
